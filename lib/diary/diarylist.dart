7a4deb75fc02a90c9c07a3caa015157651b76808
jaehyung0 <94wogud@daum.net>
2022-08-05 23:36:00 +0900

    추가

--- lib/diary/diarylist.dart
+++ lib/diary/diarylist.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class DiaryList extends StatefulWidget {
   const DiaryList({Key? key}) : super(key: key);
@@ -33,85 +34,87 @@ class _DiaryListState extends State<DiaryList> {
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Center(
-                child: StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('diary')
-                        .orderBy('time', descending: true)
-                        .snapshots(),
-                    builder: (context,
-                        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                            snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      final diaryInfo = snapshot.data!.docs;
-                      return ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: diaryInfo.length,
-                          itemBuilder: (context, index) {
-                            Timestamp time = diaryInfo[index].data()['time'];
-                            var date = DateTime.fromMillisecondsSinceEpoch(
-                                time.seconds * 1000);
-                            String title = diaryInfo[index].data()['title'];
-                            String content = diaryInfo[index].data()['content'];
-                            var image = diaryInfo[index].data()['image'];
-                            var id = diaryInfo[index].data()['userId'];
-                            final user = FirebaseAuth.instance.currentUser;
-                            if (id == user!.uid) {
-                              return InkWell(
-                                onTap: () {
-                                  Get.to(() => const DiaryDetail(), arguments: {
-                                    'title': title,
-                                    'content': content,
-                                    'date': date,
-                                    'image': image
-                                  });
-                                },
-                                child: Card(
-                                  elevation: 4,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(title,
-                                                  style: const TextStyle(
-                                                      fontSize: 30,
-                                                      color: Colors.lightBlue)),
-                                              Text(
-                                                date
-                                                    .toString()
-                                                    .substring(0, 19),
-                                                style: const TextStyle(
-                                                    fontSize: 20,
-                                                    color: Colors.red),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(height: 10),
-                                        Text(content,
-                                            style:
-                                                const TextStyle(fontSize: 25))
-                                      ],
+            child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('diary')
+                    .orderBy('time', descending: true)
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  final diaryInfo = snapshot.data!.docs;
+                  return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: diaryInfo.length,
+                      itemBuilder: (context, index) {
+                        Timestamp time = diaryInfo[index].data()['time'];
+                        var date = DateTime.fromMillisecondsSinceEpoch(
+                            time.seconds * 1000);
+                        String title = diaryInfo[index].data()['title'];
+                        String content = diaryInfo[index].data()['content'];
+                        var image = diaryInfo[index].data()['image'];
+                        var id = diaryInfo[index].data()['userId'];
+                        final user = FirebaseAuth.instance.currentUser;
+                        if (id == user!.uid) {
+                          return InkWell(
+                            onTap: () {
+                              Get.to(() => const DiaryDetail(), arguments: {
+                                'title': title,
+                                'content': content,
+                                'date': date,
+                                'image': image
+                              });
+                            },
+                            child: Card(
+                              elevation: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    AutoSizeText(title,
+                                        style: const TextStyle(
+                                            fontSize: 30,
+                                            color: Colors.lightBlue),
+                                        maxLines: 1),
+                                    const SizedBox(height: 15),
+                                    Text(
+                                        '날짜: ${date.toString().substring(0, 16)}',
+                                        style: const TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.deepOrange)),
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: Colors.lightBlue),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      width: MediaQuery.of(context).size.width -
+                                          40,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(12.0),
+                                        child: AutoSizeText(content,
+                                            minFontSize: 25,
+                                            maxLines: 8,
+                                            overflow: TextOverflow.ellipsis),
+                                      ),
                                     ),
-                                  ),
+                                  ],
                                 ),
-                              );
-                            } else {
-                              return const SizedBox();
-                            }
-                          });
-                    })),
+                              ),
+                            ),
+                          );
+                        } else {
+                          return const SizedBox();
+                        }
+                      });
+                }),
           ),
         ));
   }
